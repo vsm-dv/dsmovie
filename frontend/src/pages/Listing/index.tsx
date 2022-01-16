90b3c8d284b8ed6390c1f709d319607bd995753c
vsm-dv <valeria.mouraaa@gmail.com>
2022-01-16 17:22:39 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,13 @@
+import axios from "axios";
 import Pagination from "components/Pagination";
 import MovieCard from "pages/MovieCard";
+import { BASE_URL } from "utils/requests";
 
 function Listing() {
+
+    axios.get(`${BASE_URL}/movies?size=12&page=0`).then(response => {
+        console.log(response.data);
+    })
     return (
         <>
             <Pagination />
